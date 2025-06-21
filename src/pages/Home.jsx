34c91c38024c5b51603ad299c64a95a1e0427f3b
mayurmewada <mayurmewada00@gmail.com>
2025-06-21 23:40:17 +0530

    feat: popular products section on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getHomePageData } from "../redux/slices/homeSlice";
 import Loader from "../components/common/Loader";
+import { getFormatedAmount } from "../utils/helperFunctions";
+import Button from "../components/common/Button";
 
 const Home = () => {
     const navigate = useNavigate();
@@ -25,7 +27,7 @@ const Home = () => {
                     DEAL DECK
                 </h1>
             </section>
-            <section className="container !mt-[44px]">
+            <section className="container !py-[44px]">
                 <h4 className="text-[22px] mb-4 font-semibold text-grey-500">Shop by Categories</h4>
                 <div className="flex flex-wrap lg:flex-nowrap gap-4">
                     {loading ? (
@@ -37,6 +39,34 @@ const Home = () => {
                                 <h6 className="capitalize me-auto">{category}</h6>
                             </div>
                         ))
+                    )}
+                </div>
+            </section>
+            <section className="container !mt-[44px]">
+                <h4 className="text-[22px] mb-4 font-semibold text-grey-500">Shop by Popluar Product</h4>
+                <div className="flex flex-wrap lg:flex-nowrap gap-4">
+                    {loading ? (
+                        <Loader />
+                    ) : (
+                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5 w-full">
+                            {data?.trendingProducts?.map((product, i) => {
+                                const image = Array(product?.images?.split(","))?.[0]?.[0];
+                                return (
+                                    <div key={i} onClick={() => navigate({ pathname: "/product", search: `?id=${product._id}` })} className="border border-gray-100 shadow-elevationClose px-4 py-3 w-full h-[fit-content]">
+                                        <div className="flex flex-col space-y-4">
+                                            <div className="flex items-center justify-center aspect-square w-full">
+                                                <img className="aspect-square object-contain" src={image} alt="" />
+                                            </div>
+                                            <div className="flex flex-col py-1 gap-y-4">
+                                                <h6 className="line-clamp-2">{product.name}</h6>
+                                                <p className="text-[20px]">{getFormatedAmount(product.price)}</p>
+                                                <Button title="Add to Cart" variant="secondary" size="medium" onClick={(e) => handleAddToCart(e, product._id, product.name, product.brand, product.price)} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
                     )}
                 </div>
             </section>
